b0b86b2781589bb84cb8d468101e71fb1b4cdbe1
nolsen <nolsen@regis.edu>
2013-10-29 08:53:12 -0600

    Created New Branch

---
+++ MBS_DA_BI_Application/MBS_DA_BI_Application/Models/AXMBSBIDA.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\nolsen\Source\Repos\MSSE682\MBS_DA_BI_Application\MBS_DA_BI_Application\Models\AXMBSBIDA.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
